d49ca6451b4a4148b5aa8acce49d2ad69c3ff033
rg98098 <rg98098@gmail.com>
2023-03-26 18:33:02 +0530

    build and deployment changes

--- config/paths.js
+++ config/paths.js
@@ -1,14 +1,14 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   // root
-  root: path.resolve(__dirname, '../'),
+  root: path.resolve(__dirname, "../"),
   // Source files
-  src: path.resolve(__dirname, '../src'),
+  src: path.resolve(__dirname, "../src"),
 
   // Production build files
-  build: path.resolve(__dirname, '../dist'),
+  build: path.resolve(__dirname, "../build"),
 
   // Static files that get copied to build folder
-  public: path.resolve(__dirname, '../public')
-}
+  public: path.resolve(__dirname, "../public"),
+};
